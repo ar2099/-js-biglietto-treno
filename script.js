527f3aa069f56fd7d6ab87bcab60336e2587025d
Andrea <onemanshow2099@gmail.com>
2022-03-15 11:19:50 +0100

    02 variabile età

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const costoKilometro = 0.21;
+// const costoKilometro = 0.21;
 
-let eta = prompt("Inserisci l'età");
+// let eta = prompt("Inserisci l'età");
 
-let costoBiglietto = 5 * costoKilometro;
+// let costoBiglietto = 5 * costoKilometro;
 
-console.log( costoBiglietto)
+// console.log( costoBiglietto)
 
+let costoKm = 0.21;
+
+let eta = prompt("inserisci l'età");
+
+let km = prompt("Quanti km devi percorrere?");
+
+let variabileEta;
+
+let costoBiglietto = costoKm * km * variabileEta;
+
+console.log(costoBiglietto)
+
+if
